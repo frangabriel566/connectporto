ad2c1525945232871208984b25bbb44d9c94f43c
GABRIEL LOPES <frangabriel566@icloud.com>
2026-06-01 20:27:55 -0300

    auto: update

--- admin-bridge.js
+++ admin-bridge.js
@@ -56,10 +56,10 @@
   ];
 
   var DEFAULT_ESCRITORIOS = [
-    { cidade:'Porto',              endereco:'Av. Presidente Vargas, 193', cep:'64145-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Avenida+Presidente+Vargas+193,+Porto,+PI,+64145-000' },
-    { cidade:'N. S. dos Remédios', endereco:'Rua Marcelino Rodrigues',   cep:'64149-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Rua+Marcelino+Rodrigues,+Nossa+Senhora+dos+Remedios,+PI,+64149-000' },
-    { cidade:'Miguel Alves',       endereco:'R. São José, 85',           cep:'64130-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Rua+Sao+Jose+85,+Miguel+Alves,+PI,+64130-000' },
-    { cidade:'Barras',             endereco:'Av. Juscelino Kubitscheck', cep:'64100-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Avenida+Juscelino+Kubitscheck,+Barras,+PI,+64100-000' }
+    { cidade:'Porto-Pi',                      endereco:'Av. Presidente Vargas, 193', cep:'64145-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Avenida+Presidente+Vargas+193,+Porto,+PI,+64145-000' },
+    { cidade:'Nossa Senhora dos Remédios-Pi', endereco:'Rua Marcelino Rodrigues',   cep:'64149-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Rua+Marcelino+Rodrigues,+Nossa+Senhora+dos+Remedios,+PI,+64149-000' },
+    { cidade:'Miguel Alves-Pi',               endereco:'R. São José, 85',           cep:'64130-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Rua+Sao+Jose+85,+Miguel+Alves,+PI,+64130-000' },
+    { cidade:'Barras-Pi',                     endereco:'Av. Juscelino Kubitscheck', cep:'64100-000', uf:'PI', horario:'Seg–Sex: 08h–12h / 14h–17h | Sáb: 08h–12h | Dom: Fechado', maps:'https://maps.google.com/?q=Avenida+Juscelino+Kubitscheck,+Barras,+PI,+64100-000' }
   ];
 
   var STAR_SVG = '<svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>';
